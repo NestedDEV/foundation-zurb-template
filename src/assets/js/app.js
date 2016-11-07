2a87e5cb7427d95735408510b184acf8863ebf2f
NestedDEV <karl@nested.ie>
2016-11-07 11:34:31 +0000

    fixed top bar and offcanvas sticky functionality - still no solution for moving content when stick fires

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,3 @@
 $(document).foundation();
 
 
-$('.top-bar').on('sticky.zf.stuckto:top', function(){
-	$(this).addClass('shrink');
-}).on('sticky.zf.unstuckfrom:top', function(){
-	$(this).removeClass('shrink');
-})
